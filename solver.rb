aad23706aad90c71237b77bda40f87dcd51a4ef8
gdumani <gdumani@gmail.com>
2023-05-26 05:49:15 -0600

    Update README.md and check linters

--- solver.rb
+++ solver.rb
@@ -1,19 +1,20 @@
 class Solver
-  def factorial arg
-    raise ArgumentError if arg < 0
+  def factorial(arg)
+    raise ArgumentError if arg.negative?
+
     fac = 1
-    arg.times { |i| fac*=i+1 } if arg > 0
-    fac 
+    arg.times { |i| fac *= i + 1 } if arg.positive?
+    fac
   end
 
-  def reverse arg
-    arg.split('').reverse.join
+  def reverse(arg)
+    arg.chars.reverse.join
   end
 
-  def fizzbuzz arg
+  def fizzbuzz(arg)
     str = ''
-    str += 'fizz' if arg % 3 == 0 
-    str += 'buzz' if arg % 5 == 0  
+    str += 'fizz' if (arg % 3).zero?
+    str += 'buzz' if (arg % 5).zero?
     str.empty? ? arg.to_s : str
   end
 end
